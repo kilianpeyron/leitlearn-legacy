2e0ecab4232987ca4108448e2396f1039c743361
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-03-31 12:40:32 +0200

    Mise en production

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ use Cake\Database\Driver\Mysql;
 use Cake\Log\Engine\FileLog;
 use Cake\Mailer\Transport\MailTransport;
 
-define('APP_ENV', getenv('APP_ENV') ?: 'development');
+define('APP_ENV', getenv('APP_ENV') ?: 'production');
 return [
     /*
      * Debug Level:
