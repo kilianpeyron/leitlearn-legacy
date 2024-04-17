9af687e0641af9e2f1779e5def8f8b223337cd4a
Kilian Peyron <kilianpeyron@MacBook-Air-de-Kilian.local>
2024-04-17 18:01:03 +0100

    dashboard refresh

--- templates/Dashboard/refresh.php
+++ templates/Dashboard/refresh.php
@@ -1,4 +1,13 @@
 <main class="refresh">
+    <div class="new-ui">
+        <div class="switch-container">
+            <h5 class="action-name"><?= __('Activer la nouvelle interface') ?></h5>
+            <label class="switch">
+                <input type="checkbox" name="status">
+                <span></span>
+            </label>
+        </div>
+    </div>
     <div class="refresh-grid">
         <div class="grid-item grid-packets panel-left">
             <div class="item-header">
@@ -65,6 +74,29 @@
             <div class="grid-item grid-actions panel-right-bottom">
                 <div class="item-header">
                     <h2>Moi (<?= $user_data['username'] ?>)</h2>
+                </div>
+                <div class="item-body">
+                    <div class="user">
+                        <?= $this->Html->image('/img/user_profile_pic/'. $user_data['profile_picture'], ['class' => 'avatar', 'alt' => 'Profile Picture']) ?>
+                        <h3 class="name"><?= $user_data['username'] ?></h3>
+                    </div>
+                    <div class="actions">
+                        <?= $this->Html->link(
+                            'Mon profil',
+                            '/users/view/' . $user_data["user_uid"],
+                            ['class' => 'action', 'escapeTitle' => false]
+                        ); ?>
+                        <?= $this->Html->link(
+                            'Paramètres de compte',
+                            '/settings',
+                            ['class' => 'action', 'escapeTitle' => false]
+                        ); ?>
+                        <?= $this->Html->link(
+                            'Déconnexion',
+                            '',
+                            ['class' => 'action primary', 'escapeTitle' => false]
+                        ); ?>
+                    </div>
                 </div>
             </div>
         </div>
